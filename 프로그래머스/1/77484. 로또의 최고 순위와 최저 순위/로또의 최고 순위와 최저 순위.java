3f05f25f9f46a13949b92be024ef0ae7731fdcd3
김기수 <103559017+Lunarltn@users.noreply.github.com>
2025-08-07 09:44:52 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.03 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = new int[2];
+        int zero = 0;
+        for (int i = 0; i < lottos.length; i++) {
+            if (lottos[i] == 0) {
+                zero++;
+                continue;
+            }
+            for (int j = 0; j < win_nums.length; j++) {
+                if (lottos[i] == win_nums[j]) {
+                    answer[0]++;
+                    answer[1]++;
+                    continue;
+                }
+            }
+        }
+        answer[0] += zero;
+        answer[0] = 7 - answer[0] > 6 ? 6 : 7 - answer[0];
+        answer[1] = 7 - answer[1] > 6 ? 6 : 7 - answer[1];
+        return answer;
+    }
+}
